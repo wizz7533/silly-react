ab220cd39f924c6c7e0aaee07b6bc0f430e17196
Denis Fortict <d.fortict@vivit.fr>
2021-08-04 18:29:37 +0200

    Todolist styling

--- 10-Simple-TodoList/todolist/src/index.js
+++ 10-Simple-TodoList/todolist/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import TodoList from './TodoList';
 
 let destination = document.querySelector("#root");
 
 ReactDOM.render(
   <div>
-    <p>Hello</p>
+    <TodoList />
   </div>,
   destination
 );
