6aa12445135cb5a14385304b4e7e20b0ce3e2503
Denis Fortict <d.fortict@vivit.fr>
2021-08-04 15:18:33 +0200

    TodoList - initialisation

---
+++ 10-Simple-TodoList/todolist/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let destination = document.querySelector("#root");
+
+ReactDOM.render(
+  <div>
+    <p>Hello</p>
+  </div>,
+  destination
+);
