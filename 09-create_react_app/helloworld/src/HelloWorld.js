ec28a79987c1f0d28b102a195b8d4d86c9206fe0
Denis Fortict <d.fortict@vivit.fr>
2021-08-04 14:53:10 +0200

    Create React App - initialisation

---
+++ 09-create_react_app/helloworld/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div className="HelloContainer">
+                <h1>Hello World !</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
---
+++ 09-create_react_app/helloworld/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld'
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+);
+
